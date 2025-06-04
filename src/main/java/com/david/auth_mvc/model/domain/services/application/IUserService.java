d93a6b7e0828fb5b50427bd4ddbe9a43b8153411
David Rodriguez Badillo <devdevbadillo@gmail.com>
2025-06-04 09:41:21 -0600

    Delete src/main/java/com/david/auth_mvc/model/domain/services/application directory

--- src/main/java/com/david/auth_mvc/model/domain/services/application/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.david.auth_mvc.model.domain.services.application;
-
-import com.david.auth_mvc.model.domain.dto.response.MessageResponse;
-
-public interface IUserService {
-
-    MessageResponse signOut(String accessTokenId);
-}
