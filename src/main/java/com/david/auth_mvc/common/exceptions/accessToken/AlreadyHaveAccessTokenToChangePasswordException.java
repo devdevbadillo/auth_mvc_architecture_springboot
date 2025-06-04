69bdee87481e197a005d45f92e3de5fe76b819fb
David Rodriguez Badillo <devdevbadillo@gmail.com>
2025-06-04 09:41:37 -0600

    Delete src/main/java/com/david/auth_mvc/common/exceptions/accessToken directory

--- src/main/java/com/david/auth_mvc/common/exceptions/accessToken/AlreadyHaveAccessTokenToChangePasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.david.auth_mvc.common.exceptions.accessToken;
-
-public class AlreadyHaveAccessTokenToChangePasswordException extends Exception {
-
-    public AlreadyHaveAccessTokenToChangePasswordException(String message) {
-        super(message);
-    }
-}
